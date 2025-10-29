ca6dab69e81d7fe2c6571625edb0388036708b02
rnoyer <romain.noyer@gmail.com>
2025-10-29 01:02:51 +0100

    add home link to header

--- src/components/sections/SectionHeader.vue
+++ src/components/sections/SectionHeader.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
 <template>
   <div id="header" class="flex items-center px-8 glass h-12">
-    <h1 class="font-ff text-xl text-slate-600 uppercase">Fast Forecast >></h1>
+    <RouterLink to="/home">
+      <h1 class="font-ff text-xl text-slate-600 uppercase">Fast Forecast >></h1>
+    </RouterLink>
   </div>
 </template>
 <style scoped></style>
