675b8b940fc2b9cdf0d9fd13a642b0119134200d
rnoyer <romain.noyer@gmail.com>
2025-10-31 16:45:15 +0100

    fix kboard bug on mobile

--- src/components/sections/SectionForm.vue
+++ src/components/sections/SectionForm.vue
@@ -16,8 +16,17 @@ async function handleSubmit() {
 
 <template>
   <form @submit.prevent="handleSubmit" id="form" class="flex my-12">
-    <label for='location' hidden>Enter a location</label>
-    <input v-model="locationInput" type='text' name='location' placeholder='Enter a location' class=" rounded-none rounded-l-sm input input-lg text-xl join-item"/>
+    <label for='location' class="sr-only">Enter a location</label>
+    <input
+      v-model="locationInput"
+      id="location"
+      type='text'
+      name='location'
+      placeholder='Enter a location'
+      inputmode="search"
+      autocomplete="on"
+      class=" rounded-none rounded-l-sm input input-lg text-xl join-item"
+    />
     <input type=submit value='>>' class=" btn btn-lg btn-soft rounded-none rounded-r-sm btn-square font-ff text-xl text-slate-600 uppercase join-item" />
   </form>
 </template>
